b9cf25e6247442c26fbbcb09b7cd711a5736858c
Mark Szulc <mszulc@adobe.com>
2024-09-12 21:25:41 +1000

    initial map

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { loadScript, sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+// add more delayed functionality here
+const map = document.querySelector('#locator-map');
+if (map) {
+  loadScript('/blocks/map/location-init.js', { defer: true });
+}
