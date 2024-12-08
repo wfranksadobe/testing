91b370f8159da72307e5cb98014c1913068b52c6
Liviu Chis <chis@adobe.com>
2024-12-08 12:36:07 -0800

    Added blog block

---
+++ blocks/blog/blog.js
@@ -0,0 +1,44 @@
+export default async function decorate(block) {
+  const props = [...block.children];
+  const path = props[1]?.textContent.trim();
+  const highlightIndex = Number(props[2]?.textContent.trim());
+  const cachebuster = Math.floor(Math.random() * 1000);
+
+  const main = document.createElement('div');
+  main.innerHTML = "Loading Rates";
+
+  if(!path) {
+    return;
+  }
+
+  const url = `https://publish-p130746-e1298459.adobeaemcloud.com/graphql/execute.json/securbank/ArticleByPath;path=${path};variation=main?ts=${cachebuster}`;
+
+  const resp = await fetch(url);
+  if (!resp.ok) {
+    return;
+  }
+
+  const respJson = await resp.json();
+  const articleJson = respJson.data.accountRatesByPath.item;
+
+  // Clear main div
+  main.innerHTML = "";
+
+  const header = document.createElement('h1');
+  const author = document.createElement('h2');
+  const content = document.createElement('div');
+
+  header.classList.add("header");
+  author.classList.add("author");
+  content.classList.add("content");
+
+  header.innerHTML = articleJson.headline;
+  author.innerHTML = `By ${articleJson.author.name}`;
+  content.innerHTML = articleJson.main.html;
+  
+  main.append(header, author, content);
+
+  block.closest('.rates-wrapper').classList.add(path ? "single" : "list");
+  block.closest('.rates').replaceWith(...main.childNodes);
+  //block.append(main);
+}
