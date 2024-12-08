749141b83b09427c5099c48599dee8082caf3de8
Liviu Chis <chis@adobe.com>
2024-12-08 12:43:09 -0800

    Added blog block

--- blocks/blog/blog.js
+++ blocks/blog/blog.js
@@ -1,7 +1,6 @@
 export default async function decorate(block) {
   const props = [...block.children];
-  const path = props[1]?.textContent.trim();
-  const highlightIndex = Number(props[2]?.textContent.trim());
+  const path = props[0]?.textContent.trim();
   const cachebuster = Math.floor(Math.random() * 1000);
 
   const main = document.createElement('div');
