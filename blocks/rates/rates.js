dde5ed178680f3b1e6d5482e85a26cd2154a6783
Liviu Chis <chis@adobe.com>
2024-12-09 05:23:37 -0800

    Added blog block

--- blocks/rates/rates.js
+++ blocks/rates/rates.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
   const props = [...block.children];
   const path = props[1]?.textContent.trim();
-  const highlightIndex = Number(props[2]?.textContent.trim());
+  const highlightIndex = Number(props[3]?.textContent.trim());
   const cachebuster = Math.floor(Math.random() * 1000);
 
   const main = document.createElement('div');
